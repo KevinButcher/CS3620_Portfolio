a02bbdfe458f36a21b2b66f3dcbb0a83627a10da
KevinButcher <kevinbutcher@mail.weber.edu>
2025-02-19 16:02:38 -0700

    more changes

--- CS3620_Portfolio/Users/models.py
+++ CS3620_Portfolio/Users/models.py
@@ -6,6 +6,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='profilepic.jpg', upload_to='profile_pictures')
     location = models.CharField(max_length=100)
+    # linkedin_url = models.URLField(max_length=400, null=True, blank=True)
+    # github_url = models.URLField(max_length=400, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
