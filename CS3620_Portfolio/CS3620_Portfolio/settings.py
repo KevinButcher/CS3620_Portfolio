3e7edf0757a34b4c8d3c23491870af8e39ea283f
KevinButcher <kevinbutcher@mail.weber.edu>
2025-05-05 16:27:49 -0600

    turn off debug

--- CS3620_Portfolio/CS3620_Portfolio/settings.py
+++ CS3620_Portfolio/CS3620_Portfolio/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&ln9cp@5kv#)*d!m1&andq(1zr7kwv_i6b=ti$7q4#5fk@1o8h')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['kevinbutcherportfolio.onrender.com']
 
