fa9159399c9931cf0097e3cf38ab8630300e8dc5
KevinButcher <kevinbutcher@mail.weber.edu>
2025-05-05 14:18:22 -0600

    clean up tracked files now in .gitignore

--- CS3620_Portfolio/CS3620_Portfolio/settings.py
+++ CS3620_Portfolio/CS3620_Portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&ln9cp@5kv#)*d!m1&andq(1zr7kwv_i6b=ti$7q4#5fk@1o8h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["your-app.onrender.com"]
 
